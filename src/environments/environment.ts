7a2f151f17eb72e8389124eae38eee37d8cf4788
Umesh <globaltechumesh11@gmail.com>
2025-12-15 14:00:07 +0545

    EndPoint Changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   appwrite: {
+    //I have changed the end point from dev to prod here 
     endpoint: 'https://fra.cloud.appwrite.io/v1',
     projectId: '691d8e970006515cb4c6',
     appwriteProjectName: 'IT-ASCOL',
